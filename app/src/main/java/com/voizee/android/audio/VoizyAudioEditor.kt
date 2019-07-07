4d74bd9e8f41dea0cea1fa9a133615a435700a80
Jani Tarkiainen <j.tartsi@gmail.com>
2019-07-07 12:48:47 +0300

    Cleaning up

---
+++ app/src/main/java/com/voizee/android/audio/VoizyAudioEditor.kt
@@ -0,0 +1,12 @@
+package com.voizee.android.audio
+
+class VoizyAudioEditor {
+
+    public fun startRecording() {
+
+    }
+
+    public fun stopRecording() {
+
+    }
+}
--- app/src/main/java/com/voizee/android/ui/AudioRecordingFragment.kt
+++ app/src/main/java/com/voizee/android/ui/AudioRecordingFragment.kt
@@ -8,11 +8,15 @@ import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import com.github.piasy.audio_mixer.AudioBuffer
+import com.github.piasy.audio_mixer.AudioMixer
+import com.github.piasy.audio_mixer.MixerConfig
+import com.github.piasy.audio_mixer.MixerSource
 import com.voizee.android.R
 import kotlinx.android.synthetic.main.audio_recording_fragment.*
+import java.util.*
 
 class AudioRecordingFragment: Fragment() {
 
