bfd3bb5ec7185246da853997bbcceb45746187a0
Jani Tarkiainen <j.tartsi@gmail.com>
2019-07-24 11:40:58 +0300

    VoizyRecorder

--- app/src/main/java/com/voizy/android/audio/VoizyRecorder.kt
+++ app/src/main/java/com/voizy/android/audio/VoizyRecorder.kt
@@ -1,8 +1,39 @@
 package com.voizy.android.audio
 
-class VoizyRecorder {
+import android.content.Context
+import android.media.MediaRecorder
+import timber.log.Timber
+import java.io.IOException
+import java.util.Date
 
-    private fun startRecording() {
-        // TODO to implement
+class VoizyRecorder(val context: Context) {
+
+    private var mediaRecorder: MediaRecorder? = null
+
+    public fun startRecording() {
+
+        val filename = "${context.filesDir}/voizy_${Date().time}"
+
+        mediaRecorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setOutputFile(filename)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+
+            try {
+                prepare()
+            } catch (e: IOException) {
+                Timber.e(e, "prepare() failed")
+            }
+            start()
+        }
+    }
+
+    public fun stopRecording() {
+        mediaRecorder?.apply {
+            stop()
+            release()
+        }
+        mediaRecorder = null
     }
 }
