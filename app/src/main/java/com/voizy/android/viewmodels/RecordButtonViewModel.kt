7f86deff7c35a473c32278672ce3df8387a041a8
Jani Tarkiainen <j.tartsi@gmail.com>
2019-07-25 11:17:16 +0300

    Thread handling for fetching filepath

--- app/src/main/java/com/voizy/android/viewmodels/RecordButtonViewModel.kt
+++ app/src/main/java/com/voizy/android/viewmodels/RecordButtonViewModel.kt
@@ -2,7 +2,10 @@ package com.voizy.android.viewmodels
 
 import android.content.Context
 import androidx.lifecycle.ViewModel
+import com.uber.autodispose.autoDisposable
 import com.voizy.android.audio.VoizyRecorder
+import io.reactivex.Completable
+import io.reactivex.schedulers.Schedulers
 import java.util.Date
 
 class RecordButtonViewModel(
@@ -11,8 +14,16 @@ class RecordButtonViewModel(
 ) : ViewModel() {
 
     public fun startRecording() {
-        val filename = "${context.filesDir}/voizy_${Date().time}"
-        voizyRecorder.startRecording(filename)
+
+        val completable = Completable.fromAction {
+            val filename = "${context.filesDir}/voizy_${Date().time}"
+            voizyRecorder.startRecording(filename)
+        }.subscribeOn(Schedulers.io())
+
+        completable.apply {
+            autoDisposable(this)
+            subscribe()
+        }
     }
 
     public fun stopRecording() {
