28b6d11169fb1fb4707ff44e77d2767cbf59f321
Jani Tarkiainen <j.tartsi@gmail.com>
2019-07-25 12:35:38 +0300

    Voizy player

--- app/src/main/java/com/voizy/android/audio/VoizyPlayer.kt
+++ app/src/main/java/com/voizy/android/audio/VoizyPlayer.kt
@@ -1,3 +1,27 @@
 package com.voizy.android.audio
 
-class VoizyPlayer
+import android.media.MediaPlayer
+import timber.log.Timber
+import java.io.IOException
+
+class VoizyPlayer {
+
+    private val player = MediaPlayer()
+
+    public fun prepare(filepath: String) {
+        try {
+            player.setDataSource(filepath)
+            player.prepare()
+        } catch (e: IOException) {
+            Timber.e(e, "prepare() failed")
+        }
+    }
+
+    public fun play() {
+        player.start()
+    }
+
+    public fun release() {
+        player.release()
+    }
+}
