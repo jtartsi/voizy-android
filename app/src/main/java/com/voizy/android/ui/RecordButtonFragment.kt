ed0b95eb75e1a34f0efa5918eda8544bf6f5c65f
Jani Tarkiainen <j.tartsi@gmail.com>
2019-07-25 10:40:36 +0300

    Moving permission request to RecordButtonFragment

--- app/src/main/java/com/voizy/android/ui/RecordButtonFragment.kt
+++ app/src/main/java/com/voizy/android/ui/RecordButtonFragment.kt
@@ -1,7 +1,10 @@
 package com.voizy.android.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.view.HapticFeedbackConstants
 import android.view.LayoutInflater
 import android.view.MotionEvent
@@ -10,18 +13,25 @@ import android.view.ViewGroup
 import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import com.uber.autodispose.android.lifecycle.AndroidLifecycleScopeProvider
+import com.uber.autodispose.autoDisposable
 import com.voizy.android.R
 import com.voizy.android.viewmodels.RecordButtonViewModel
+import io.reactivex.Completable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import org.koin.android.ext.android.inject
 
 @SuppressWarnings("ClickableViewAccessibility")
 class RecordButtonFragment : Fragment() {
 
     private val viewModel: RecordButtonViewModel by inject<RecordButtonViewModel>()
+    private val scopeProvider by lazy { AndroidLifecycleScopeProvider.from(viewLifecycleOwner) }
 
     companion object {
         public val TAG = RecordButtonFragment::class.java.simpleName
         private const val ANIMATION_DELAY = 200L
+        private const val REQUEST_RECORD_AUDIO_PERMISSION = 100
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -46,6 +56,30 @@ class RecordButtonFragment : Fragment() {
                     false
                 }
             }
+        }
+
+        Completable
+            .fromAction {
+                requestPermissions(
+                    arrayOf(Manifest.permission.RECORD_AUDIO),
+                    REQUEST_RECORD_AUDIO_PERMISSION
+                )
+            }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .autoDisposable(scopeProvider)
+            .subscribe()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION &&
+            grantResults[0] != PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.d(RecordingOverlayFragment.TAG, "onRequestPermissionsResult() NO permission")
+            fragmentManager!!.popBackStack(RecordingOverlayFragment.TAG, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+        } else {
+            Log.d(RecordingOverlayFragment.TAG, "onRequestPermissionsResult() permission")
         }
     }
 
--- app/src/main/java/com/voizy/android/ui/RecordingOverlayFragment.kt
+++ app/src/main/java/com/voizy/android/ui/RecordingOverlayFragment.kt
@@ -1,22 +1,15 @@
 package com.voizy.android.ui
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-
 import com.uber.autodispose.android.lifecycle.AndroidLifecycleScopeProvider
 import com.uber.autodispose.autoDisposable
 import com.voizy.android.R
 import com.voizy.android.viewmodels.RecordingOverlayViewModel
-import io.reactivex.Completable
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 import org.koin.android.ext.android.inject
 import timber.log.Timber
 
@@ -26,20 +19,8 @@ class RecordingOverlayFragment : Fragment() {
     private val scopeProvider by lazy { AndroidLifecycleScopeProvider.from(viewLifecycleOwner) }
 
     companion object {
-        private const val REQUEST_RECORD_AUDIO_PERMISSION = 100
-        public val TAG = RecordingOverlayFragment::class.java.simpleName
-    }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION &&
-            grantResults[0] != PackageManager.PERMISSION_GRANTED
-        ) {
-            Log.d(TAG, "onRequestPermissionsResult() NO permission")
-            fragmentManager!!.popBackStack(TAG, FragmentManager.POP_BACK_STACK_INCLUSIVE)
-        } else {
-            Log.d(TAG, "onRequestPermissionsResult() permission")
-        }
+        public val TAG = RecordingOverlayFragment::class.java.simpleName
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -58,17 +39,5 @@ class RecordingOverlayFragment : Fragment() {
             .subscribe {
                 Timber.d("recording event $it")
             }
-
-        Completable
-            .fromAction {
-                requestPermissions(
-                    arrayOf(Manifest.permission.RECORD_AUDIO),
-                    REQUEST_RECORD_AUDIO_PERMISSION
-                )
-            }
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .autoDisposable(scopeProvider)
-            .subscribe()
     }
 }
