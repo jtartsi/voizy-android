86c74e91a2ab20fd7fe6e02a9556a261ab22409b
Jani Tarkiainen <j.tartsi@gmail.com>
2019-07-26 10:27:29 +0300

    Replaying audio in preview works now

--- app/src/main/java/com/voizy/android/audio/VoizyPlayer.kt
+++ app/src/main/java/com/voizy/android/audio/VoizyPlayer.kt
@@ -1,23 +1,17 @@
 package com.voizy.android.audio
 
 import android.media.MediaPlayer
-import timber.log.Timber
-import java.io.IOException
 
 class VoizyPlayer {
 
-    private val player = MediaPlayer()
-
     public fun play(filepath: String) {
-        try {
-            player.setDataSource(filepath)
-            player.prepare()
-            player.start()
-        } catch (e: IOException) {
-            Timber.e(e, "prepare() failed")
-        }
-        player.setOnCompletionListener {
-            player.release()
+        MediaPlayer().apply {
+            setDataSource(filepath)
+            prepare()
+            start()
+            setOnCompletionListener {
+                it.release()
+            }
         }
     }
 }
