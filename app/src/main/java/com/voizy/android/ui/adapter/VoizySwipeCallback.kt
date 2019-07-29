71a19d7f2d9517586cc5cae032fc2e6cdf43cada
Jani Tarkiainen <j.tartsi@gmail.com>
2019-07-29 13:46:16 +0300

    working on swipes

--- app/src/main/java/com/voizy/android/ui/adapter/VoizySwipeCallback.kt
+++ app/src/main/java/com/voizy/android/ui/adapter/VoizySwipeCallback.kt
@@ -1,12 +1,26 @@
 package com.voizy.android.ui.adapter
 
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import com.voizy.android.R
 
-class VoizySwipeCallback
-    : ItemTouchHelper.SimpleCallback(
+class VoizySwipeCallback(private val context: Context) : ItemTouchHelper.SimpleCallback(
     0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
 ) {
+
+    private lateinit var deleteDrawable: Drawable
+    private lateinit var shareDrawable: Drawable
+    private lateinit var background: ColorDrawable
+
+    init {
+        shareDrawable = context.getDrawable(R.drawable.ic_send_white)
+        background = ColorDrawable(context.getColor(android.R.color.holo_orange_dark))
+    }
 
     override fun onMove(
         recyclerView: RecyclerView,
@@ -17,6 +31,35 @@ class VoizySwipeCallback
     }
 
     override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+        val position = viewHolder.adapterPosition
+        when (direction) {
+            ItemTouchHelper.LEFT -> {
+                Snackbar.make(viewHolder.itemView, "Deleting $position", Snackbar.LENGTH_SHORT).show()
+            }
+            ItemTouchHelper.RIGHT -> {
+                Snackbar.make(viewHolder.itemView, "Sharing $position", Snackbar.LENGTH_SHORT).show()
+            }
+        }
+
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onChildDraw(
+        c: Canvas,
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        dX: Float,
+        dY: Float,
+        actionState: Int,
+        isCurrentlyActive: Boolean
+    ) {
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+
+        if (dX > 0) { // Swiping to right
+        } else if (dX < 0) { // Swiping to left
+        } else { // View is unSwiped
+        }
+        val itemView = viewHolder.itemView
+        val backgoundCornerOffset = 20
     }
 }
