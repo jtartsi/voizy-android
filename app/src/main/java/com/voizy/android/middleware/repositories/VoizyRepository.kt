4b088436c36c0f326091b611069bb198334f4364
Jani Tarkiainen <j.tartsi@gmail.com>
2019-09-28 18:27:05 +0700

    Cleaning up

--- app/src/main/java/com/voizy/android/middleware/repositories/VoizyRepository.kt
+++ app/src/main/java/com/voizy/android/middleware/repositories/VoizyRepository.kt
@@ -11,7 +11,6 @@ import com.voizy.android.utils.collectionChange
 import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.PublishSubject
-import timber.log.Timber
 import java.io.File
 
 class VoizyRepository(
@@ -52,44 +51,12 @@ class VoizyRepository(
     fun searchVoizys(searchKeyword: String): Observable<List<Voizy>> {
         return voizysSearch.find(searchKeyword)
             .map { it.result() }
-            .doOnNext {
-                Timber.d("search-voizys path ${it.path}")
-            }
             .collectionChange()
-            .filter {
-                Timber.d("search-voizys filter size ${it.documentChanges.size}")
-                it.documentChanges.size != 0
-            }
             .map { it.toObjects(FirestoreVoizySearchResult::class.java) }
-            .filter {
-                Timber.d("search-voizys filter2 size ${it.size}")
-                it.size != 0
-            }
+            .filter { it.size != 0 }
             .map { it.first() }
-            .doOnNext {
-                Timber.d("search-voizys hits ${it.hits}")
-                Timber.d("search-voizys hits.voizys ${it.hits.hits}")
-                Timber.d("search-voizys total value ${it.hits.total.value}")
-            }
             .map { it.getVoizys() }
-            .doOnNext {
-                Timber.d("search-voizys voizys size ${it.size}")
-            }
-            .doOnNext {
-                for (voizy in it) {
-                    Timber.d("search-voizys voizy: ${voizy.name}}")
-                }
-            }
     }
-
-    // fun getVoizys(searchKeyword: String = ""): Observable<List<Voizy>> {
-    //     return voizys.getVoizys()
-    //         .flatMap { Observable.fromIterable(it) }
-    //         .map { it.toVoizy() }
-    //         .toList()
-    //         .toObservable()
-    //         .withErrorHandling(TAG, "failed to fetch Voizys")
-    // }
 
     fun getFileUrl(firestorePath: String): Observable<String> {
         return voizyStorage.getDownloadUri(firestorePath)
