b4ce9d16070155a4c9ae4a4f7bdc83513d0220d2
Jani Tarkiainen <j.tartsi@gmail.com>
2019-09-29 11:49:24 +0700

    Removed the debounce

--- app/src/main/java/com/voizy/android/viewmodels/MainFragmentViewModel.kt
+++ app/src/main/java/com/voizy/android/viewmodels/MainFragmentViewModel.kt
@@ -12,9 +12,7 @@ import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.BehaviorSubject
 import io.reactivex.subjects.PublishSubject
-import timber.log.Timber
 import java.io.File
-import java.util.concurrent.TimeUnit
 
 class MainFragmentViewModel(
     private val voizyRepository: VoizyRepository,
@@ -29,9 +27,6 @@ class MainFragmentViewModel(
 
     private val searchVoizysRequest = PublishSubject.create<String>()
     private val voizysStream = searchVoizysRequest
-        .doOnNext { Timber.d("search-voizys BEFORE debounce") }
-        .debounce(300, TimeUnit.MILLISECONDS)
-        .doOnNext { Timber.d("search-voizys AFTER debounce") }
         .observeOn(Schedulers.io())
         .flatMap { voizyRepository.searchVoizys(it) }
         .withErrorHandling(TAG, "Searching voizys failed")
