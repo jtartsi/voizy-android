2c86c57ea72eb8aa88d4bff4324f28dd6892ced1
Jani Tarkiainen <j.tartsi@gmail.com>
2019-11-20 14:46:17 +0700

    Cleaning up

--- app/src/main/java/com/voizy/android/ui/MainActivity.kt
+++ app/src/main/java/com/voizy/android/ui/MainActivity.kt
@@ -14,8 +14,7 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // TODO file-upload validate data here
+        
         if (intent.action == Intent.ACTION_SEND) {
             val recordingFragment = RecordingFragment()
 
