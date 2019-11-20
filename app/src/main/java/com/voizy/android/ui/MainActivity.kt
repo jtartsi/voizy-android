1e31d45a16219686cb2cdc433233425a0bc0ccc1
Jani Tarkiainen <j.tartsi@gmail.com>
2019-11-20 14:51:45 +0700

    Cleaning up

--- app/src/main/java/com/voizy/android/ui/MainActivity.kt
+++ app/src/main/java/com/voizy/android/ui/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
         if (intent.action == Intent.ACTION_SEND) {
             val recordingFragment = RecordingFragment()
 
