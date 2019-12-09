37b44eb07afec0af15d5c06f241fc490d4646ce6
Jani Tarkiainen <j.tartsi@gmail.com>
2019-12-09 10:47:11 +0700

    FileInputStream to InputStream

--- app/src/main/java/com/voizy/android/middleware/firebase/VoizyFirebaseStorage.kt
+++ app/src/main/java/com/voizy/android/middleware/firebase/VoizyFirebaseStorage.kt
@@ -9,6 +9,7 @@ import com.voizy.android.utils.withErrorHandling
 import io.reactivex.Observable
 import java.io.File
 import java.io.FileInputStream
+import java.io.InputStream
 import java.net.URLEncoder
 import java.util.Date
 
@@ -27,7 +28,7 @@ class VoizyFirebaseStorage(
         val encodedFirebaseFileName = URLEncoder.encode(firebaseFileName, "UTF-8")
 
         val uploadRef = storageRef.child(VOIZYS_PATH).child(encodedFirebaseFileName)
-        val stream = FileInputStream(File(voizy.localPath))
+        val stream = FileInputStream(File(voizy.localPath)) as InputStream
 
         return uploadRef.putStream(stream).toObservable()
             .map {
