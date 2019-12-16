6451d9d587b63395fe07de8bbcf52b285bb2dfe2
Jani Tarkiainen <j.tartsi@gmail.com>
2019-12-16 13:21:21 +0700

    VoizyDetailsFragment skeleton

--- app/src/main/java/com/voizy/android/ui/fragment/VoizyDetailsFragment.kt
+++ app/src/main/java/com/voizy/android/ui/fragment/VoizyDetailsFragment.kt
@@ -1,4 +1,46 @@
 package com.voizy.android.ui.fragment
 
-class VoizyDetailsFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.voizy.android.R
+
+class VoizyDetailsFragment : BaseFragment() {
+
+    companion object {
+        public val TAG = RecordingFragment::class.java.simpleName
+    }
+
+    override fun getFragmentTag(): String {
+        return TAG
+    }
+
+    override fun onBackPressed() {
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.voizy_details_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initDetails()
+        initPlayback()
+        initShare()
+    }
+
+    private fun initDetails() {
+    }
+
+    private fun initPlayback() {
+    }
+
+    private fun initShare() {
+    }
 }
