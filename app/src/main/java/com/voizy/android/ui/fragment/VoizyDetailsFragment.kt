67cabfa00afe0d130544986309561e609b6c83e6
Jani Tarkiainen <j.tartsi@gmail.com>
2019-12-16 18:01:03 +0700

    VoizyDetailsFragment Actions

--- app/src/main/java/com/voizy/android/ui/fragment/VoizyDetailsFragment.kt
+++ app/src/main/java/com/voizy/android/ui/fragment/VoizyDetailsFragment.kt
@@ -4,11 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.snackbar.Snackbar
+import com.jakewharton.rxbinding2.view.RxView
 import com.uber.autodispose.autoDisposable
 import com.voizy.android.R
+import com.voizy.android.audio.PlaybackEvent
+import com.voizy.android.middleware.firebase.models.Voizy
+import com.voizy.android.ui.widget.PlayPauseButton
 import com.voizy.android.utils.getScopeProvider
 import com.voizy.android.viewmodels.VoizyDetailsViewModel
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.functions.Consumer
+import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.row_item_voizy.*
 import kotlinx.android.synthetic.main.voizy_details_fragment.*
 import org.koin.android.ext.android.inject
@@ -16,6 +23,7 @@ import org.koin.android.ext.android.inject
 class VoizyDetailsFragment : BaseFragment() {
 
     private val viewModel: VoizyDetailsViewModel by inject()
+    private val shareRequests = PublishSubject.create<Voizy>()
 
     companion object {
         public val TAG = RecordingFragment::class.java.simpleName
@@ -60,8 +68,37 @@ class VoizyDetailsFragment : BaseFragment() {
     }
 
     private fun initPlayback() {
+        RxView.clicks(btn_playback)
+            .flatMap { viewModel.togglePlay() }
+            .autoDisposable(getScopeProvider())
+            .subscribe()
+
+        viewModel.playbackEvents
+            .observeOn(AndroidSchedulers.mainThread())
+            .autoDisposable(getScopeProvider())
+            .subscribe {
+                if (it.playbackEvent == PlaybackEvent.START) {
+                    btn_playback.state = PlayPauseButton.State.STOP_ICON
+                } else if (it.playbackEvent == PlaybackEvent.STOP) {
+                    btn_playback.state = PlayPauseButton.State.PLAY_ICON
+                }
+            }
     }
 
     private fun initShare() {
+        shareRequests
+            .doOnNext(showSharingToast())
+            .autoDisposable(getScopeProvider())
+            .subscribe { viewModel.share(context!!) }
+    }
+
+    private fun showSharingToast(): Consumer<Voizy> {
+        return Consumer {
+            Snackbar.make(
+                view!!,
+                getString(R.string.voizy_sharing, it.name),
+                Snackbar.LENGTH_LONG
+            ).show()
+        }
     }
 }
--- app/src/main/java/com/voizy/android/viewmodels/VoizyDetailsViewModel.kt
+++ app/src/main/java/com/voizy/android/viewmodels/VoizyDetailsViewModel.kt
@@ -1,15 +1,25 @@
 package com.voizy.android.viewmodels
 
+import android.content.Context
+import com.voizy.android.audio.AudioPlayer
+import com.voizy.android.audio.PlaybackInfo
 import com.voizy.android.middleware.firebase.models.Voizy
 import com.voizy.android.middleware.local.LocalFileManager
 import com.voizy.android.middleware.repositories.VoizyRepository
+import com.voizy.android.utils.ShareManager
 import com.voizy.android.utils.withErrorHandling
 import io.reactivex.Observable
+import java.io.File
 
 class VoizyDetailsViewModel(
     private val voizyRepository: VoizyRepository,
-    private val localFileManager: LocalFileManager
+    private val voizyPlayer: AudioPlayer,
+    private val localFileManager: LocalFileManager,
+    private val shareManager: ShareManager
 ) : DisposingViewModel() {
+
+    val playbackEvents: Observable<PlaybackInfo>
+        get() = voizyPlayer.getPlaybackEvents()
 
     companion object {
         private val TAG = VoizyDetailsViewModel::class.java.simpleName
@@ -17,6 +27,28 @@ class VoizyDetailsViewModel(
 
     fun getLastVoizyToBeSaved(): Observable<Voizy> {
         return voizyRepository.lastVoizyToBeSaved()
+    }
+
+    fun togglePlay(): Observable<PlaybackInfo> {
+        return voizyPlayer
+            .togglePlay(voizyRepository.getTempFilePath())
+            .withErrorHandling(TAG, "Failed to togglePlay on preview voizy")
+    }
+
+    fun stopPlayback(): Observable<Int> {
+        return voizyPlayer
+            .stop()
+            .withErrorHandling(TAG, "Failed to stopPlayback on preview voizy")
+    }
+
+    fun share(context: Context) {
+        voizyRepository.lastVoizyToBeSaved()
+            .doOnNext { voizy ->
+                shareManager.startVoizyShare(context, voizy, File(voizy.localPath))
+            }
+            .withErrorHandling(TAG, "Failed to share voizy")
+            .subscribe()
+            .autoDispose()
     }
 
     fun getAudioFileLengthInSeconds(path: String): Observable<Int> {
