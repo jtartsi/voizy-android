6910095a02df78bcd28483da600d11ec1a7f2367
Jani Tarkiainen <j.tartsi@gmail.com>
2019-12-18 18:14:45 +0700

    Video import works

--- app/src/main/java/com/voizy/android/utils/SupportedFileTypes.kt
+++ app/src/main/java/com/voizy/android/utils/SupportedFileTypes.kt
@@ -11,11 +11,12 @@ class SupportedFileTypes {
         private const val AUDIO_M4A = "audio/m4a"
         private const val AUDIO_MPEG_4 = "audio/mpeg-4"
         private const val AUDIO_X_M4A = "audio/x-m4a"
+        private const val VIDEO_ANY = "video/*"
 
         fun toArray(): Array<String> {
             return arrayOf(
                 AUDIO_MPEG, AUDIO_MP3, AUDIO_AAC, AUDIO_X_WAV, AUDIO_AMR,
-                AUDIO_MP4, AUDIO_M4A, AUDIO_MPEG_4, AUDIO_X_M4A
+                AUDIO_MP4, AUDIO_M4A, AUDIO_MPEG_4, AUDIO_X_M4A, VIDEO_ANY
             )
         }
     }
