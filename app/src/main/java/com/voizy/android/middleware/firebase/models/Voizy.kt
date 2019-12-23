68cb9ea53bb6c8465323fd146fe83c00ad6fa600
Jani Tarkiainen <j.tartsi@gmail.com>
2019-12-23 11:56:29 +0700

    Cleaning up

--- app/src/main/java/com/voizy/android/middleware/firebase/models/Voizy.kt
+++ app/src/main/java/com/voizy/android/middleware/firebase/models/Voizy.kt
@@ -21,8 +21,7 @@ data class Voizy(
     val localeLang: String = "",
     val localeCountry: String = ""
 ) {
-
-    // TODO fixnow remove the local fields from being set to backend
+    
     @get:Exclude
     var hashTags: String = ""
         get() {
