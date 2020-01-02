10a2fd22eb0c94259b84f19c2f0f69aee6eeb9e0
Jani Tarkiainen <j.tartsi@gmail.com>
2020-01-02 13:18:07 +0700

    Cleaning up

--- app/src/main/java/com/voizy/android/ui/fragment/AudioClipFragment.kt
+++ app/src/main/java/com/voizy/android/ui/fragment/AudioClipFragment.kt
@@ -223,40 +223,13 @@ class AudioClipFragment : BaseFragment() {
     }
 
     private fun formatAudioTime(timeInMillis: Long): String {
-        Timber.d("formatAudioTime() timeInMillis $timeInMillis")
         val stringBuilder = StringBuilder()
-        if (timeInMillis > 60000) {
-            Timber.d("formatAudioTime() >60s timeString $stringBuilder")
+        return if (timeInMillis > 60000) {
             val timeFormat = SimpleDateFormat("mm:ss.S")
-            return stringBuilder.append(timeFormat.format(timeInMillis)).toString()
+            stringBuilder.append(timeFormat.format(timeInMillis)).toString()
         } else {
             val timeFormat = SimpleDateFormat("s.S")
-            return stringBuilder.append(timeFormat.format(timeInMillis)).append("s").toString()
+            stringBuilder.append(timeFormat.format(timeInMillis)).append("s").toString()
         }
     }
-
-    // private fun formatAudioTime(timeInMillis: Long): String {
-    //     Timber.d("formatAudioTime() timeInMillis $timeInMillis")
-    //     val stringBuilder = StringBuilder()
-    //     if (timeInMillis > 60000) {
-    //         Timber.d("formatAudioTime() >60s")
-    //         val minutes = timeInMillis / 60000
-    //         stringBuilder.append(minutes).append("m ")
-    //         Timber.d("formatAudioTime() >60s timeString $stringBuilder")
-    //     }
-    //
-    //     val timeFormat = SimpleDateFormat("s.S")
-    //     val formattedTime = timeFormat.format(timeInMillis)
-    //     Timber.d("formatAudioTime() formattedTime $formattedTime")
-    //     stringBuilder.append(formattedTime)
-    //     Timber.d("formatAudioTime() final timeString $stringBuilder")
-    //     return stringBuilder.toString()
-    // }
-
-    // private fun formatAudioTime(timeInMillis: Long): String {
-    //     Timber.d("formatAudioTime() timeInMillis $timeInMillis")
-    //
-    //     val minuteFormat = SimpleDateFormat("mm:ss.S")
-    //     return minuteFormat.format(timeInMillis)
-    // }
 }
