99f89a349ab14249c05adb5bf1f554cb517c1709
Jani Tarkiainen <j.tartsi@gmail.com>
2020-01-14 18:23:34 +0700

    Some changes to the trending headline

--- app/src/main/java/com/voizy/android/ui/fragment/LibraryFragment.kt
+++ app/src/main/java/com/voizy/android/ui/fragment/LibraryFragment.kt
@@ -2,14 +2,13 @@ package com.voizy.android.ui.fragment
 
 import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
+import com.jakewharton.rxbinding2.widget.RxTextView
 import com.uber.autodispose.autoDisposable
 import com.voizy.android.R
 import com.voizy.android.audio.PlaybackEvent
@@ -19,12 +18,17 @@ import com.voizy.android.ui.adapter.VoizyListAdapter
 import com.voizy.android.ui.adapter.VoizyViewHolder
 import com.voizy.android.utils.NetworkState
 import com.voizy.android.utils.getScopeProvider
+import com.voizy.android.utils.toPair
 import com.voizy.android.viewmodels.LibraryFragmentViewModel
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.library_fragment.*
 import org.koin.android.ext.android.inject
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 class LibraryFragment : BaseFragment() {
 
@@ -40,6 +44,7 @@ class LibraryFragment : BaseFragment() {
     private lateinit var voizyListAdapter: VoizyListAdapter
     private val shareRequests = PublishSubject.create<Voizy>()
     private val clipBoardRequests = PublishSubject.create<Voizy>()
+    private lateinit var searchTextChanges: Observable<CharSequence>
 
     companion object {
         val TAG = LibraryFragment::class.java.simpleName
@@ -61,6 +66,10 @@ class LibraryFragment : BaseFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         voizyRecyclerView = view.findViewById(R.id.rv_voizy_list)
+        searchTextChanges = RxTextView.textChanges(et_search)
+            .toFlowable(BackpressureStrategy.LATEST)
+            .toObservable()
+            .share()
     }
 
     override fun onStart() {
@@ -69,6 +78,7 @@ class LibraryFragment : BaseFragment() {
         initLoader()
         initVoizyListing()
         initSearch()
+        initLibraryHeader()
         initShare()
         initCopyToClipBoard()
         initPrivacyPolicy()
@@ -148,6 +158,7 @@ class LibraryFragment : BaseFragment() {
             .observeOn(AndroidSchedulers.mainThread())
             .autoDisposable(getScopeProvider())
             .subscribe {
+                Timber.d("search-change initResultsState() $it")
                 if (it == NetworkState.LOADING) {
                     layout_no_results.visibility = View.GONE
                 } else {
@@ -157,29 +168,53 @@ class LibraryFragment : BaseFragment() {
             }
     }
 
+    private fun initLibraryHeader() {
+        Observable.combineLatest(
+            searchTextChanges.delay(1, TimeUnit.SECONDS),
+            viewModel.initialLoading, toPair()
+        )
+            .observeOn(AndroidSchedulers.mainThread())
+            .autoDisposable(getScopeProvider())
+            .subscribe {
+                Timber.d("search-change initLibraryHeader().subscribe() ${it.first}, ${it.second}")
+                if (it.first.isNullOrEmpty() && it.second != NetworkState.LOADING) {
+                    Timber.d("search-change initLibraryHeader().subscribe() visible")
+                    tv_library_headline.visibility = View.VISIBLE
+                } else {
+                    Timber.d("search-change initLibraryHeader().subscribe() gone")
+                    tv_library_headline.visibility = View.GONE
+                }
+            }
+    }
+
     private fun initSearch() {
-        et_search.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(
-                searchText: CharSequence?,
-                start: Int,
-                before: Int,
-                count: Int
-            ) {
+        searchTextChanges
+            .autoDisposable(getScopeProvider())
+            .subscribe { searchText ->
+                Timber.d("search-change initSearch().subscribe() $searchText")
                 voizyRecyclerView.scrollToPosition(0)
                 voizyListAdapter.submitList(null)
                 viewModel.loadVoizys(searchText.toString())
-
-                val headlineVisiblity =
-                    if (searchText.isNullOrEmpty()) View.VISIBLE else View.GONE
-                tv_library_headline.visibility = headlineVisiblity
             }
-        })
+        // et_search.addTextChangedListener(object : TextWatcher {
+        //     override fun afterTextChanged(s: Editable?) {
+        //     }
+        //
+        //     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        //     }
+        //
+        //     override fun onTextChanged(
+        //         searchText: CharSequence?,
+        //         start: Int,
+        //         before: Int,
+        //         count: Int
+        //     ) {
+        //         voizyRecyclerView.scrollToPosition(0)
+        //         voizyListAdapter.submitList(null)
+        //         viewModel.loadVoizys(searchText.toString())
+        //
+        //     }
+        // })
     }
 
     private fun initCopyToClipBoard() {
