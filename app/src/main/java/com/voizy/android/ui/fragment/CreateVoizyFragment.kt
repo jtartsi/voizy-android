4af438f92d4da324f05597341ef8698212c3f540
Jani Tarkiainen <j.tartsi@gmail.com>
2020-02-17 13:37:06 +0700

    Some self made layouts for the pager

---
+++ app/src/main/java/com/voizy/android/ui/fragment/CreateVoizyFragment.kt
@@ -0,0 +1,33 @@
+package com.voizy.android.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CreateVoizyFragment : BaseFragment() {
+
+    companion object {
+        val TAG = CreateVoizyFragment::class.java.simpleName
+    }
+
+    override fun getFragmentTag(): String {
+        return TAG
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+}
